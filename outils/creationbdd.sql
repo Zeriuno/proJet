f0bb33a7202849301849fada363a343e97d9f268
Zeriuno <d.pitrolo@gmx.com>
2016-07-30 03:10:32 +0200

    BDD java partout

--- outils/creationbdd.sql
+++ outils/creationbdd.sql
@@ -4,7 +4,7 @@ Commande pour se connecter en root:
 mysql -u root -p
 Ensuite il faut saisir le mot de passe.
 */
-CREATE DATABASE javadb; --on créé une nouvelle base de données
-CREATE USER 'java'@'localhost' IDENTIFIED BY 'javapassword'; --on créé un nouvel utilisateur et on donne son mot de passe
-GRANT ALL PRIVILEGES ON javadb.* TO 'java'@'localhost'; --on donne à l'utilisateur les droits pour travailler sur la base que l'on vient de créer
+CREATE DATABASE java; --on créé une nouvelle base de données
+CREATE USER 'java'@'localhost' IDENTIFIED BY 'java'; --on créé un nouvel utilisateur et on donne son mot de passe
+GRANT ALL PRIVILEGES ON java.* TO 'java'@'localhost'; --on donne à l'utilisateur les droits pour travailler sur la base que l'on vient de créer
 FLUSH PRIVILEGES; --on met à jour les droits
