56711aef1718c059b69915340ccbc4da5ac0f7ef
patrickrivera93 <patrickrivera93@gmail.com>
2016-08-02 22:19:46 +0200

    fenetre final evenement et personne

--- code/FSaisieEven.java
+++ code/FSaisieEven.java
@@ -3,8 +3,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,19 +13,10 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class FSaisieEven extends JFrame{
-	private JPanel pan=new JPanel();
-	private JPanel calendrier=new JPanel();
 	private JPanel container = new JPanel();
-    private JButton boutonG=new JButton("< Précedant");
-    private JButton boutonD=new JButton("Prochain >");
-    private JButton boutonAjoutPers=new JButton("+Personne");
-    private JButton boutonAjoutEven=new JButton("+Evenement");
     private JButton b1 =new JButton("ENREGISTRER"); // bouton placé en private et pas 
-    //dans la méthode car sinon on n'arrive pas à dire au bouton qu'il est écouté
     
-    /*
-     * Champs de saisie
-     */
+    // Champs de saisie
     
     private JTextField nomEven = new JTextField("RDV"); // Valeur par défaut
     private JLabel labelnomEven = new JLabel("Titre événement:"); // texte avant le champ
@@ -43,66 +32,25 @@ public class FSaisieEven extends JFrame{
     private JLabel labelheureFin = new JLabel("Heure de fin:"); // texte avant le champ
     
 	public FSaisieEven() {
-		this.setTitle("Bouton");
-	    this.setSize(1200, 300);
+		this.setTitle("Evenement");
+	    this.setSize(600, 150);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setLocationRelativeTo(null);
-	  //On définit le layout à utiliser sur le content pane
-
-	    //this.setLayout(new GridLayout());
-	   
-	    //BorderLayout
-	    this.getContentPane().add(boutonG, BorderLayout.SOUTH);
-	    //Grid  Layout
-	    this.setLayout(new GridLayout(1, 2));
-	    this.getContentPane().add(boutonG);
-	    this.getContentPane().add(boutonD);
-	    
-	    //Boxlayout
-
-	    //JPanel b1 = new JPanel();
-	    //On définit le layout en lui indiquant qu'il travaillera en ligne
-	    //b1.setLayout(new BoxLayout(b1, BoxLayout.LINE_AXIS));
-	    //b1.add(new JButton("Bouton 1"));
-	    
+	  	    
 	    b1.addActionListener(new BoutonListener()); //on indique au bouton qu'il est écouté
 
-	    JPanel b2 = new JPanel();
-	    //Idem pour cette ligne
-	    b2.setLayout(new BoxLayout(b2, BoxLayout.LINE_AXIS));
-	    b2.add(new JButton("Bouton 2"));
-	    b2.add(new JButton("Bouton 3"));
-
-	    JPanel b3 = new JPanel();
-	    //Idem pour cette ligne
-	    b3.setLayout(new BoxLayout(b3, BoxLayout.LINE_AXIS));
-	    b3.add(new JButton("Bouton 4"));
-	    b3.add(new JButton("Bouton 5"));
-	    b3.add(new JButton("Bouton 6"));
-
-	    JPanel b4 = new JPanel();
-	    //On positionne maintenant ces trois lignes en colonne
-	    b4.setLayout(new BoxLayout(b4, BoxLayout.PAGE_AXIS));
-	    b4.add(b1);
-	    b4.add(b2);
-	    b4.add(b3);
-
+	    
 	    container.setBackground(Color.white);
 	    container.setLayout(new BorderLayout());
 	    JPanel top = new JPanel();
 	    Font police = new Font("Arial", Font.BOLD, 14);
-	    //nomEven.setFont(police); // style
-	    nomEven.setPreferredSize(new Dimension(150, 30));
-	    //nomEven.setForeground(Color.BLUE); // style
+	    nomEven.setPreferredSize(new Dimension(220, 30));
 	    top.add(labelnomEven);
 	    top.add(nomEven);
-	  //lieu.setFont(police); // style
-	    lieu.setPreferredSize(new Dimension(150, 30));
-	    //nomEven.setForeground(Color.BLUE); // style
+	    lieu.setPreferredSize(new Dimension(200, 30));
 	    top.add(labellieu);
 	    top.add(lieu);
-	    invite.setPreferredSize(new Dimension(150, 30));
-	    //nomEven.setForeground(Color.BLUE); // style
+	    invite.setPreferredSize(new Dimension(120, 30));
 	    top.add(labelinvite);
 	    top.add(invite);
 	    dateEven.setPreferredSize(new Dimension(90, 30));
@@ -110,19 +58,13 @@ public class FSaisieEven extends JFrame{
 	    top.add(dateEven);
 	    top.add(labelheureDeb);
 	    top.add(heureDeb);
-	    heureDeb.setPreferredSize(new Dimension(60, 30));
+	    heureDeb.setPreferredSize(new Dimension(50, 30));
 	    top.add(labelheureFin);
 	    top.add(heureFin);
-	    //heureFin.setFont(police);
-	    heureFin.setPreferredSize(new Dimension(60, 30));
-	    //heureFin.setForeground(Color.BLUE);
+	    heureFin.setPreferredSize(new Dimension(50, 30));
+	    top.add(b1);
 	    	    
-	    
-	    container.add(top, BorderLayout.NORTH);
-	    this.setContentPane(container);
-	    
-	    this.getContentPane().add(b4);
-	    this.setVisible(true);
+	    this.setContentPane(top);
 	    this.setVisible(true);
 	}
 	
