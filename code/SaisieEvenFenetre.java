7b9f713ff158ca701232e2ca839bf5ea2ce600c4
patrickrivera93 <patrickrivera93@gmail.com>
2016-08-06 21:54:54 +0200

    elimination du texte de remplissage des champs lors du clic de la souris

--- code/SaisieEvenFenetre.java
+++ code/SaisieEvenFenetre.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -24,7 +26,6 @@ import javax.swing.JTextField;
 
 
 public class SaisieEvenFenetre extends JFrame{
-	private JPanel container = new JPanel(); 
     private JButton b1 =new JButton("ENREGISTRER"); // bouton enregistrement 
     
     // Champs de saisie
@@ -50,36 +51,112 @@ public class SaisieEvenFenetre extends JFrame{
 	    
 	    
 	    //Remarque :
-	    //boutonlistenner permet d'indiquer le bouton d'action 
-	    //actiolisener permet de utiliser la fonction actionPerformed 
+	    //boutonlistener permet d'indiquer le bouton d'action 
+	    //actionlistener permet de utiliser la fonction actionPerformed 
 	    //actionPerformed permet aux bouton d'appeller l'action    
 	    b1.addActionListener(new BoutonListener()); 
 	    //on indique au bouton qu'il est écouté
 
-	    //I ça serve absoluement rien !!!!
-	    /*container.setBackground(Color.red);
-	    container.setLayout(new BorderLayout());*/
 	    //II Div pour les boutons et les panels
 	    JPanel top = new JPanel();
 	    Font police = new Font("Arial", Font.BOLD, 14);
 	    nomEven.setPreferredSize(new Dimension(220, 30));
+	    // permet de vider le champ de saisie lorsqu'un clic à lieu afin de pouvoir saisir les données souhaitées
+	    nomEven.addMouseListener(new MouseListener(){
+	        public void mouseReleased(MouseEvent e) {}         
+	        public void mousePressed(MouseEvent e) {}          
+	        public void mouseExited(MouseEvent e) {}           
+	        public void mouseEntered(MouseEvent e) {}          
+	    	public void mouseClicked(MouseEvent e){
+	    		JTextField nomEven = ((JTextField)e.getSource());
+	    		nomEven.setText("");
+	    		nomEven.getFont().deriveFont(Font.PLAIN);
+	    		nomEven.setForeground(Color.black);
+	    		nomEven.removeMouseListener(this);
+	    	}
+	    });
 	    top.add(labelnomEven);
 	    top.add(nomEven);
 	    lieu.setPreferredSize(new Dimension(200, 30));
+	    lieu.addMouseListener(new MouseListener(){
+	        public void mouseReleased(MouseEvent e) {}         
+	        public void mousePressed(MouseEvent e) {}          
+	        public void mouseExited(MouseEvent e) {}           
+	        public void mouseEntered(MouseEvent e) {}          
+	    	public void mouseClicked(MouseEvent e){
+	    		JTextField lieu = ((JTextField)e.getSource());
+	    		lieu.setText("");
+	    		lieu.getFont().deriveFont(Font.PLAIN);
+	    		lieu.setForeground(Color.black);
+	    		lieu.removeMouseListener(this);
+	    	}
+	    });
 	    top.add(labellieu);
 	    top.add(lieu);
 	    invite.setPreferredSize(new Dimension(120, 30));
+	    invite.addMouseListener(new MouseListener(){
+	        public void mouseReleased(MouseEvent e) {}         
+	        public void mousePressed(MouseEvent e) {}          
+	        public void mouseExited(MouseEvent e) {}           
+	        public void mouseEntered(MouseEvent e) {}          
+	    	public void mouseClicked(MouseEvent e){
+	    		JTextField invite = ((JTextField)e.getSource());
+	    		invite.setText("");
+	    		invite.getFont().deriveFont(Font.PLAIN);
+	    		invite.setForeground(Color.black);
+	    		invite.removeMouseListener(this);
+	    	}
+	    });
 	    top.add(labelinvite);
 	    top.add(invite);
 	    dateEven.setPreferredSize(new Dimension(90, 30));
+	    dateEven.addMouseListener(new MouseListener(){
+	        public void mouseReleased(MouseEvent e) {}         
+	        public void mousePressed(MouseEvent e) {}          
+	        public void mouseExited(MouseEvent e) {}           
+	        public void mouseEntered(MouseEvent e) {}          
+	    	public void mouseClicked(MouseEvent e){
+	    		JTextField dateEven = ((JTextField)e.getSource());
+	    		dateEven.setText("");
+	    		dateEven.getFont().deriveFont(Font.PLAIN);
+	    		dateEven.setForeground(Color.black);
+	    		dateEven.removeMouseListener(this);
+	    	}
+	    });
 	    top.add(labeldateEven);
 	    top.add(dateEven);
 	    top.add(labelheureDeb);
 	    top.add(heureDeb);
 	    heureDeb.setPreferredSize(new Dimension(50, 30));
+	    heureDeb.addMouseListener(new MouseListener(){
+	        public void mouseReleased(MouseEvent e) {}         
+	        public void mousePressed(MouseEvent e) {}          
+	        public void mouseExited(MouseEvent e) {}           
+	        public void mouseEntered(MouseEvent e) {}          
+	    	public void mouseClicked(MouseEvent e){
+	    		JTextField heureDeb = ((JTextField)e.getSource());
+	    		heureDeb.setText("");
+	    		heureDeb.getFont().deriveFont(Font.PLAIN);
+	    		heureDeb.setForeground(Color.black);
+	    		heureDeb.removeMouseListener(this);
+	    	}
+	    });
 	    top.add(labelheureFin);
 	    top.add(heureFin);
 	    heureFin.setPreferredSize(new Dimension(50, 30));
+	    heureFin.addMouseListener(new MouseListener(){
+	        public void mouseReleased(MouseEvent e) {}         
+	        public void mousePressed(MouseEvent e) {}          
+	        public void mouseExited(MouseEvent e) {}           
+	        public void mouseEntered(MouseEvent e) {}          
+	    	public void mouseClicked(MouseEvent e){
+	    		JTextField heureFin = ((JTextField)e.getSource());
+	    		heureFin.setText("");
+	    		heureFin.getFont().deriveFont(Font.PLAIN);
+	    		heureFin.setForeground(Color.black);
+	    		heureFin.removeMouseListener(this);
+	    	}
+	    });
 	    top.add(b1);
 	    	    
 	    this.setContentPane(top);
