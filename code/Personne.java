2f35462f2b2359914425822a645d919b3b10a5bf
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-07 22:56:35 +0200

    Traitement de la base de données dans la classe Personne

--- code/Personne.java
+++ code/Personne.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class Personne {
 
@@ -15,11 +19,41 @@ public class Personne {
 			this.nomPers = nomPers;
 			this.prenomPers=prenomPers;
 			this.emailPers=emailPers;
-			Bdmanager.ajoutPers(nomPers, prenomPers,emailPers);
+			// Bdmanager.ajoutPers(nomPers, prenomPers,emailPers);  // Vieille méthode qui reposait sur le Bdmanager, 
+																	// obsolète avec la factorisation du code de connexion à la base de données
+																	// et le recours donc ici à this.ajoutPers
+			this.ajoutPers(nomPers, prenomPers, emailPers) ;
 			
 		}
 		
-		
-
+		private void ajoutPers(String nomPers, String pernomPers, String emailPers)
+		{
+			Connection connexionbd = BDConnect.getConnect() ;
+			PreparedStatement pstmnt ;
+			try
+			{
+				String requete = "INSERT INTO `personne` (`nomPers`,`prenomPers`,`mailPers`) VALUES (?, ?, ?)";
+				pstmnt = connexionbd.prepareStatement(requete);
+				pstmnt.setString(1, nomPers);
+				pstmnt.setString(2, prenomPers);
+				pstmnt.setString(3, emailPers);
+				pstmnt.executeQuery();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				try
+				{
+					connexionbd.close();
+				}
+				catch (SQLException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
