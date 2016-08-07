9253898bef843f8c4c5c2978638fc7c5984e79d4
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-07 22:17:29 +0200

    PreparedStatement et Statement non nécessaires

--- code/BDConnect.java
+++ code/BDConnect.java
@@ -1,9 +1,11 @@
-import java.beans.Statement       ;
 import java.sql.Connection        ;
 import java.sql.DriverManager     ;
-import java.sql.PreparedStatement ;
-// import java.sql.SQLException      ; // Nécessaire si on a recours à SQLException pour un e.printStackTrace() qui renverrait un message d'erreur plus explicite, utile au debug
 import javax.swing.JOptionPane    ;
+/*
+import java.sql.PreparedStatement ; // Les statements sont traités dans les classes, pas ici
+import java.beans.Statement       ; // Les statements sont traités dans les classes, pas ici
+import java.sql.SQLException      ; // Nécessaire si on a recours à SQLException pour un e.printStackTrace() qui renverrait un message d'erreur plus explicite, utile au debug
+*/
 
 
 public class BDConnect {
